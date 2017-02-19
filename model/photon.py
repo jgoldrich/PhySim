014d4360f4238ec4c53c940eaf7541f251b78528
rossch2 <rossschneider2@gmail.com>
2017-02-19 00:31:44 -0600

    added mass_obj to photon inputs

--- model/photon.py
+++ model/photon.py
@@ -3,12 +3,18 @@
 
 def Class Photon:
 
-    def __init__(self, mass):
+    def __init__(self, mass, mass_obj):
+		"""
+			inputs:
+			mass (particle, at initialization)
+			mass_obj (Mass object that the photon is associated with)
+		"""
 
         self.strong = true
         self.em = 0
         self.weak = false
         self.gravity = mass
+		self.mass_obj = mass_obj
         
         self.tangent = false 
 

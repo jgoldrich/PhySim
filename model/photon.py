f71b4f53feaa630c8c1695c99966d1d91a78ba0a
samkaufman01 <samkaufman01@gmail.com>
2017-02-19 02:56:28 -0500

    Added tangent_mass information, possible scheme for determining spawn photon position

--- model/photon.py
+++ model/photon.py
@@ -1,17 +1,25 @@
-
+import interactLaw as iL
 
 
 def Class Photon:
 
-    def __init__(self, mass):
+    def __init__(self, mass, tangent_mass, photon_num):
 
         self.strong = true
         self.em = 0
         self.weak = false
         self.gravity = mass
         
-        self.tangent = false 
+        if tangent_mass == None:
+            self.tangent = false 
+        else:
+            self.tangent = true
+            self.tangent_mass = tangent_mass
+            self.tangent_photon = tangent_mass.photons[photon_num]
+
 
     def interact(self):
         
+        if self.tangent == true:
 
+        

24a51a5db5a260c404caecd2c1ed9aa8f36cbd58
samkaufman01 <samkaufman01@gmail.com>
2017-02-18 22:15:32 -0600

    Created transform method in Mass; set T1, T2 and T3 mats; update a; transpose if axis is negative.

--- model/mass.py
+++ model/mass.py
@@ -1,5 +1,5 @@
 import numpy as np
-import 
+from sets import Set
 
 
 
@@ -9,14 +9,47 @@ def Class Mass:
     def __init__(self, mass):
     
         self.mass = mass
-        self.a = [0, 1, 2, 3, 4, 5]
-        
-
+        self.a = np.array([0, 1, 2, 3, 4, 5])
 
     def transform(self, axis):
+    
+    """ 
+        Performs the rotation transformation on photon array
+        inputs: axis (element of [-3 -2 -1 1 2 3])
+    """
 
         T = np.zeros((6,6), dtype=np.int8)
-        assert type(axis) is 
+        assert type(axis) is in [-3, -2, -1, 1, 2, 3]
 
         if abs(axis) == 1:
-        
+            
+            T[0,0] = 1
+            T[4,1] = 1
+            T[1,2] = 1
+            T[2,3] = 1
+            T[3,4] = 1
+            T[5,5] = 1
+
+        if abs(axis) == 2:
+
+            T[1,0] = 1
+            T[5,1] = 1
+            T[2,2] = 1
+            T[0,3] = 1
+            T[4,4] = 1
+            T[3,5] = 1
+
+        if abs(axis) == 3:
+
+            T[2,0] = 1
+            T[1,1] = 1
+            T[5,2] = 1
+            T[3,3] = 1
+            T[0,4] = 1
+            T[4,5] = 1
+
+        if axis > 0:
+            self.a = np.dot(np.a, T)
+        else:
+            self.a = np.dot(np.a, np.transpose(T))
+

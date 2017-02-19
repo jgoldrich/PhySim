b1a8c267881c9467edfd79185e7d0eabaf043b15
samkaufman01 <samkaufman01@gmail.com>
2017-02-19 05:40:34 -0500

    Filled in some shitty shitty bullshit for dummy function

--- model/interactLaw.py
+++ model/interactLaw.py
@@ -1,6 +1,21 @@
+import numpy as np
 
+def dummy(photon1, photon2):
 
-def dummy(p1, p2):
-    pass
+    if photon1.strong == True and photon2.strong == True:
+        diff = photon1.mass.mass - photon2.mass.mass
+        if diff > 0:
+            photon1.mass.mass += np.log(diff)
+            photon2.mass.mass -= np.log(diff)
+
+    if photon1.em and photon2.em:
+        f = photon1.em * photon2.em
+        if f > 0:
+            photon1.mass.mass += np.log(f)
+            photon2.mass.mass += np.log(f)
+        if f < 0:
+            photon1.mass.mass -= np.log(f)
+            photon2.mass.mass -= np.log(f)
+    
     
 

3c384be3c9814b11267c0841fd6b8178eb33cd2a
samkaufman01 <samkaufman01@gmail.com>
2017-02-18 21:39:03 -0600

    Beginning of mass.py and photon.py classes, added ./model/*.swp to .gitignore

---
+++ model/mass.py
@@ -0,0 +1,22 @@
+import numpy as np
+import 
+
+
+
+
+def Class Mass:
+
+    def __init__(self, mass):
+    
+        self.mass = mass
+        self.a = [0, 1, 2, 3, 4, 5]
+        
+
+
+    def transform(self, axis):
+
+        T = np.zeros((6,6), dtype=np.int8)
+        assert type(axis) is 
+
+        if abs(axis) == 1:
+        
---
+++ model/photon.py
@@ -0,0 +1,17 @@
+
+
+
+def Class Photon:
+
+    def __init__(self):
+
+        self.strong = true
+        self.em = false
+        self.weak = false
+        self.gravity = false
+        
+        self.tangest = false
+
+    def interact(self):
+        
+

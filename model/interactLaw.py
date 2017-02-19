c348474987ed3a01cf14f40c5996001311c22c4a
samkaufman01 <samkaufman01@gmail.com>
2017-02-19 13:15:48 -0500

    Added more force functions

--- model/interactLaw.py
+++ model/interactLaw.py
@@ -8,6 +8,7 @@ def dummy(photon1, photon2):
             photon1.mass.mass += np.log(diff)
             photon2.mass.mass -= np.log(diff)
 
+
     if photon1.em and photon2.em:
         f = photon1.em * photon2.em
         if f > 0:
@@ -17,5 +18,20 @@ def dummy(photon1, photon2):
             photon1.mass.mass -= np.log(f)
             photon2.mass.mass -= np.log(f)
     
+def strong(p1,p2):
+
+    pass
     
+
+def em(p1,p2):
+
+
+    pass
+
+def all_forces(photon1, photon2)
+
+    if photon1.strong and photon2.strong:
+        strong(photon1, photon2)
+    if photon1.em and photon2.em:
+        em(photon1, photon2)
 
